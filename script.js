0447c58504c9a29628858c58befb989135d79ad6
melvinvargas1608 <melvinvargas144@gmail.com>
2023-06-19 19:03:47 -0600

    Mi primer pagina web en github

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+            classList.toggle("nav-links-responsive")
+    })
+
+function showAlert() {
+    alert('¡Hola, esta es una alerta desde JavaScript!');
+}
+                      
